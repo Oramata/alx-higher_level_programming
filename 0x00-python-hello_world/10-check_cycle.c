65b3c3831b0d28076ad9171fba118f9b72b0508c
Oramata <matabogeoratile@gmail.com>
2024-01-06 05:13:43 -0800

    Task 10 and list.h debug and update

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,20 +8,18 @@
  */
 int check_cycle(listint_t *list)
 {
-    listint_t *slow = list;
-    listint_t *fast = list;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-    while (slow && fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
 
-        if (slow == fast)
-        {
-            return (1); // Cycle found
-        }
-    }
-
-    return (0); // No cycle
+		if (slow == fast)
+		{
+			return (1); /* Cycle found */
+		}
+	}
+	return (0); /* No cycle */
 }
-
