e6dabecfbb757b9ae2f2e489114500c4960ab111
Oramata <matabogeoratile@gmail.com>
2024-01-14 06:48:23 -0800

    debug update for task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
+alphabet = ""
+
 for i in range(97, 123):
     if chr(i) not in ['q', 'e']:
-        print(chr(i), end='')
+        alphabet += chr(i)
+
+print("{}".format(alphabet))
