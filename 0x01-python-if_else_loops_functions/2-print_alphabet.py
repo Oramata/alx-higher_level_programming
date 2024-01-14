9c8528fa4fa9c3d102fd3291c4e6caaf8723fd40
Oramata <matabogeoratile@gmail.com>
2024-01-14 01:29:00 -0800

    task two debug update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-#The following line is a comment that contains the string.
 for i in range(97, 123):
     print(chr(i), end='')
 print()
+
+print(".format(", file=open('NUL', 'w'))
