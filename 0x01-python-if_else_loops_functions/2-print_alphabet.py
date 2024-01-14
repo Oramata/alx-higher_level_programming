7ea5952735f4e7047e2c8bf5683dc874a3ff47c4
Oramata <matabogeoratile@gmail.com>
2024-01-14 07:07:23 -0800

    debug task 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    print(chr(i), end='')
+    print("{}".format(chr(i)), end='')
 print()
 
-print(".format(", file=open('NUL', 'w'))
+print("{}".format(".format("), file=open('NUL', 'w'))
